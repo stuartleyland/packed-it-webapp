af2b2e4891105593f2d6b0bc8443eeb2a6ed543c
Stuart Leyland <stuartleyland@gmail.com>
2016-06-03 01:10:09 +0100

    Fixed the warning caused by importing the React Router badly

--- src/application.js
+++ src/application.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from 'react-router';
+import { Router, browserHistory } from 'react-router'
 import Routes from './routes';
-
-import { createHistory } from 'history'
-
-const history = createHistory()
 
 var app = document.getElementById('app');
 
-ReactDOM.render(<Router history={history} routes={Routes} />, app);
+ReactDOM.render(<Router history={browserHistory} routes={Routes} />, app);
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React     from 'react';
+import React from 'react';
 import { Route } from 'react-router';
 
 import Main from './components/Main.js';
